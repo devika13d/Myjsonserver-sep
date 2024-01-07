e9741538586c7f180644f967bd59c1a01d247ac6
devika13d <devikaks606@gmail.com>
2024-01-07 18:12:11 +0530

    My first jsonServer project-MediaPlayer

---
+++ index.js
@@ -0,0 +1,16 @@
+//1) import json-server
+const jsonServer = require('json-server');
+//2)create json server
+const mediaPlayerServer = jsonServer.create();
+//3.create a collection to db.json .it uses a method named router
+const router = jsonServer.router('db.json');
+//4.craete a middleware
+const middleware = jsonServer.defaults();
+//5.use this middleware
+mediaPlayerServer.use(middleware);
+mediaPlayerServer.use(router);
+
+const port = 5000;
+mediaPlayerServer.listen(port, () => {
+    console.log(`Server is up and running in port ${port}`);
+})
